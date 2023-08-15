bbc73f3633d75ac30f3795d107ddb14b935478d5
mehedi-web2023 <courseweb38@gmail.com>
2023-08-15 19:46:19 +0600

    javaScript

---
+++ index.js
@@ -0,0 +1,88 @@
+var price = 20;
+var mobileprice= 200;
+
+// integer. Number,float,
+
+// var mobile name =2000;
+// var 30model= 200;
+// var mobile-price = 2000;
+// var "bole" = 2000;
+
+var mobile_price= 2000;
+var mobileprice= 500;
+
+
+
+var name = "mehedi hasan";
+var price = "50";
+
+
+var tumi_relation_koro =  true;
+var tumi_relation_koro =  false;
+
+
+
+// console.log(price)
+
+var egg_price = 30;
+var rice_price = 10;
+
+var total_price = egg_price + rice_price
+var total_price2 = egg_price - rice_price
+var total_price3 = egg_price * rice_price
+var total_price4 = egg_price / rice_price
+
+
+
+console.log(total_price)
+
+console.log(total_price2)
+
+console.log(total_price3)
+console.log(total_price4)
+
+var num = 5;
+var num1 =2;
+
+console.log(num % num1)
+
+var first_name = "mehedi"
+var last_name = "hasan"
+
+
+var fullname = first_name +" 012"+ last_name
+
+console.log(fullname)
+
+
+
+var number2 = 0.1
+var number3 = 0.2
+
+var total = parseFloat(number2) + number3
+
+console.log(total.toFixed(1))
+
+var tshartPrice = 200;
+
+tshartPrice = 300;
+
+console.log(tshartPrice)
+
+
+var ammu_dilo = 500;
+
+var mobile = 20;
+var ice = 35;
+ var bara= 15;
+
+ var totalKhoros = mobile + ice + bara;
+
+
+ var roilo = ammu_dilo -  totalKhoros
+ console.log(roilo)
+
+
+
+
+//  node index.js------------------file name
